10d03d6a97743a54a5fab2d91c1c984c4f2648a9
mullerro <ronald.r.mueller@siemens.com>
2021-10-11 09:14:18 +0200

    U4L5 finished

---
+++ C#/RUS/src/Unit-4/lesson-5/MovieStreaming/MovieStreaming/Actors/PlaybackStatisticsActor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Proto;
+
+namespace MovieStreaming.Actors
+{
+    class PlaybackStatisticsActor : IActor
+    {
+        private PID _moviePlayCounterActorRef;
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started msg:
+                    var props = Props.FromProducer(() => new MoviePlayCounterActor());
+                    _moviePlayCounterActorRef = context.Spawn(props);
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
